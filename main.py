8fa02656cfa751c7382896e28efbb40fd9edba8e
rizvan <rizvangaripov@gmail.com>
2024-02-17 10:20:58 +0300

    add writing urls in index.txt

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ def save_page(url, webpage_index):
         file.write(r.content.decode())
 
 
-def write_in_index(webpage_index):
+def write_in_index(webpage_index, href):
     with open('index.txt', 'a', encoding='utf-8') as file:
-        file.write(webpage_index + " \n")
+        file.write(webpage_index + " " + href + " \n")
 
 
 def crawl(page):
     os.mkdir(directory)
 
     webpage_index = 0
-    while webpage_index < 3:
+    while webpage_index < 150:
         try:
             c = urllib2.urlopen(page)
         except:
@@ -33,8 +33,9 @@ def crawl(page):
 
         for link in links:
             if 'class' in dict(link.attrs) and link['class'] == ['entry-link']:
-                save_page(link['href'], webpage_index)
-                write_in_index(str(webpage_index))
+                href = link['href']
+                save_page(href, webpage_index)
+                write_in_index(str(webpage_index), href)
                 webpage_index = webpage_index + 1
             if 'class' in dict(link.attrs) and link['class'] == ['next']:
                 page = link['href']
