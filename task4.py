205c5b1cfec2cad7f496640305612b5b3740daaa
rizvan <rizvangaripov@gmail.com>
2024-03-06 14:21:12 +0300

    added computing tf idf

--- task4.py
+++ task4.py
@@ -1,10 +1,13 @@
 import os
 import re
 
+import numpy as np
 import spacy
 from bs4 import BeautifulSoup
 from nltk import word_tokenize
 from nltk.corpus import stopwords
+
+COUNT_OF_WEBPAGES = 156
 
 
 def remove_btn_text_if_equals(url):
@@ -32,20 +35,51 @@ def get_word_frequency_dictionary():
     return word_count
 
 
-def set_idf_dictionaries():
-    idf = {}
-    count_of_webpages = 156
+def get_count_of_docs_contains_word(word):
+    count_of_docs_contains_word = 0
+    for i in range(COUNT_OF_WEBPAGES):
+        if word in word_frequency_dictionaries[i]:
+            count_of_docs_contains_word += 1
+    return count_of_docs_contains_word
 
-    for w in word_frequency_dictionaries:
-        k = 0
 
-        for i in range(count_of_webpages):
-            if w in corpus[i].split():
-                k += 1
+def compute_tf_idf_for_words():
+    for i in range(COUNT_OF_WEBPAGES):
+        for word, frequency in word_frequency_dictionaries[i].items():
+            tf = frequency / counts_of_words_in_docs[i]
 
-        idf[w] = np.log10(n_docs / k)
+            count_of_docs_contains_word = get_count_of_docs_contains_word(word)
+            idf = np.log10(COUNT_OF_WEBPAGES / count_of_docs_contains_word)
 
-        print(f'{w:>15}: {idf[w]:>10}')
+
+def create_lemma_frequency_dictionaries():
+    for i in range(COUNT_OF_WEBPAGES):
+        lemma_count = {}
+        for line_in_lemmas in lemmas:
+            count_of_lemmas = 0
+            for lemma in line_in_lemmas:
+                if lemma in word_frequency_dictionaries[i]:
+                    count_of_lemmas += word_frequency_dictionaries[i][lemma]
+            if count_of_lemmas != 0:
+                lemma_count[line_in_lemmas[0]] = count_of_lemmas
+        lemma_frequency_dictionaries.append(lemma_count)
+
+
+def get_count_of_docs_contains_lemma(lemma):
+    count_of_docs_contains_lemma = 0
+    for i in range(COUNT_OF_WEBPAGES):
+        if lemma in lemma_frequency_dictionaries[i]:
+            count_of_docs_contains_lemma += 1
+    return count_of_docs_contains_lemma
+
+
+def compute_tf_idf_for_lemmas():
+    for i in range(COUNT_OF_WEBPAGES):
+        for lemma, frequency in lemma_frequency_dictionaries[i].items():
+            tf = frequency / counts_of_words_in_docs[i]
+
+            count_of_docs_contains_lemma = get_count_of_docs_contains_lemma(lemma)
+            idf = np.log10(COUNT_OF_WEBPAGES / count_of_docs_contains_lemma)
 
 
 if __name__ == '__main__':
@@ -53,7 +87,9 @@ if __name__ == '__main__':
 
     webpage_index = 0
     word_frequency_dictionaries = []
-    idf_dictionaries = []
+    lemma_frequency_dictionaries = []
+    counts_of_words_in_docs = []
+    lemmas = []
     while webpage_index <= 155:
         all_tokens = []
         with open(os.path.join("webpages", str(webpage_index) + '.html'), 'r', encoding='utf-8') as file:
@@ -91,8 +127,14 @@ if __name__ == '__main__':
             stop_words = set(stopwords.words('russian'))
             filtered_tokens = [token for token in filtered_tokens_alpha if token.lower() not in stop_words]
             all_tokens.extend(filtered_tokens)
+            counts_of_words_in_docs.append(len(filtered_tokens))
 
             word_frequency_dictionaries.append(get_word_frequency_dictionary())
         webpage_index = webpage_index + 1
 
-    set_idf_dictionaries()
+    compute_tf_idf_for_words()
+    with open("lemmas.txt", 'r', encoding='utf-8') as lemmas_file:
+        for line in lemmas_file:
+            lemmas.append(line.split())
+    create_lemma_frequency_dictionaries()
+    compute_tf_idf_for_lemmas()
