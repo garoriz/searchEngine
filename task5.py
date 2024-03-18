1da6910a059356a107788010eefbd04784623f77
rizvan <rizvangaripov@gmail.com>
2024-03-18 16:42:13 +0300

    added vector calculating

--- task5.py
+++ task5.py
@@ -1,9 +1,14 @@
 import os
 import re
 
+import numpy
+import numpy as np
 import spacy
 from nltk import word_tokenize
 from nltk.corpus import stopwords
+from scipy import spatial
+
+COUNT_OF_WEBPAGES = 156
 
 
 def get_lemmas():
@@ -57,8 +62,54 @@ def prepare_query(query_param):
     return result
 
 
+def get_lemma_count():
+    query_lemma_count = {}
+    for lemma in query:
+        if lemma in query_lemma_count:
+            query_lemma_count[lemma] += 1
+        else:
+            query_lemma_count[lemma] = 1
+    return query_lemma_count
+
+
+def get_count_of_docs_contains_lemma(lemma):
+    count_of_docs_contains_lemma = 0
+    for index in range(COUNT_OF_WEBPAGES):
+        with open(os.path.join("tf_idf", "lemmas", "lemmas" + str(index) + ".txt"), 'r', encoding='utf-8') as file:
+            line = file.readline()
+            while line and lemma != line.split()[0]:
+                line = file.readline()
+            if line and lemma == line.split()[0]:
+                count_of_docs_contains_lemma += 1
+    return count_of_docs_contains_lemma
+
+
+def get_query_tf_idf():
+    query_lemma_count = get_lemma_count()
+    query_lemma_tfidf = {}
+    for lemma, count in query_lemma_count.items():
+        tf = count / len(query)
+        count_of_docs_contains_lemma = get_count_of_docs_contains_lemma(lemma)
+        if count_of_docs_contains_lemma == 0:
+            continue
+        idf = np.log10(COUNT_OF_WEBPAGES / count_of_docs_contains_lemma)
+        tf_idf = tf * idf
+        query_lemma_tfidf[lemma] = tf_idf
+    return query_lemma_tfidf
+
+
+def get_vector():
+    result_vector = [0 for _ in range(len(lemmas))]
+    for lemma, tf_idf in query_tf_idf.items():
+        if lemma in lemmas:
+            result_vector[lemmas.index(lemma)] = tf_idf
+    return result_vector
+
+
 if __name__ == '__main__':
     lemmas = get_lemmas()
     vectors = get_vectors()
-    query = prepare_query(input())
-    print(query)
+    query = input()
+    query = prepare_query(query)
+    query_tf_idf = get_query_tf_idf()
+    query_vector = get_vector()
