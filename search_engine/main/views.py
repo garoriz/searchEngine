3cda91fbf27b2f4833855067dd767dbdbcb99bf6
rizvan <rizvangaripov@gmail.com>
2024-03-20 16:20:13 +0300

    add form and post method

--- search_engine/main/views.py
+++ search_engine/main/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'main/webInterface.html')
+
+
+def postquery(request):
+    name = request.POST.get("query", "")
+    return HttpResponse(f"<h2>Query: {name}</h2>")
